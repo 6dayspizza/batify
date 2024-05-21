067fc7b37d9caf9f20f5d9b9d89165e121f7881d
6dayspizza <55084543+6dayspizza@users.noreply.github.com>
2024-05-21 08:42:39 +0200

    updated form.js

--- views/public/js/form.js
+++ views/public/js/form.js
@@ -63,29 +63,13 @@ function handleFormResponse(response) {
 function showform(dostuff) {
     if (dostuff == 'page1') {
         document.getElementById('page1').style.display = 'block';
-        document.getElementById('page2').style.display = 'none';
-        document.getElementById('page3').style.display = 'none';
-        document.getElementById('page4').style.display = 'none';
-    } else if (dostuff == 'page2') {
-        document.getElementById('page1').style.display = 'none';
-        document.getElementById('page2').style.display = 'block';
-        document.getElementById('page3').style.display = 'none';
-        document.getElementById('page4').style.display = 'none';
-    } else if (dostuff == 'page3') {
-        document.getElementById('page1').style.display = 'none';
-        document.getElementById('page2').style.display = 'none';
-        document.getElementById('page3').style.display = 'block';
         document.getElementById('page4').style.display = 'none';
     } else if (dostuff == 'page4') {
         document.getElementById('page1').style.display = 'none';
-        document.getElementById('page2').style.display = 'none';
-        document.getElementById('page3').style.display = 'none';
         document.getElementById('page4').style.display = 'block';
         populateSummary();
     } else if (dostuff == 'recommendation') {
         document.getElementById('page1').style.display = 'none';
-        document.getElementById('page2').style.display = 'none';
-        document.getElementById('page3').style.display = 'none';
         document.getElementById('page4').style.display = 'none';
         document.getElementById('recommendation').style.display = 'block';
     }
